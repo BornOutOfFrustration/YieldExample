5a07760f85736faf57f6f40fdcaeeb363f683fd6
gjansen <geert__jansen@hotmail.com>
2018-05-03 12:35:58 +0200

    Added simple yield example.

--- YieldExample/YieldExample/Program.cs
+++ YieldExample/YieldExample/Program.cs
@@ -10,6 +10,26 @@ namespace YieldExample
     {
         static void Main(string[] args)
         {
+            Consumer();
+        }
+
+        public static void Consumer()
+        {
+            foreach(var i in Integers())
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        public static IEnumerable<int> Integers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 4;
+            yield return 8;
+            yield return 16;
+            yield return 16777216;
         }
     }
 }
