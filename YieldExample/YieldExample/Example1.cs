f5f3844decb21b9a3f2e03ea15730a2e38f5fa01
gjansen <geert__jansen@hotmail.com>
2018-05-18 12:05:28 +0200

    + Formalized the names of the classes + Put all example code in separate classes

---
+++ YieldExample/YieldExample/Example1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldExample
+{
+    public class Example1
+    {
+        public void LoopOverYieldList()
+        {
+            foreach (var i in ListOfIntegersUsingYield())
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private IEnumerable<int> ListOfIntegersUsingYield()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 4;
+            yield return 8;
+            yield return 16;
+            yield return 16777216;
+        }
+    }
+}
